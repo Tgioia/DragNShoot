7400ea4fedb62df58e9a0832bc9294023be36a9d
Tgioia <103372001+Tgioia@users.noreply.github.com>
2024-02-19 19:21:05 +0100

    skin selection
    (da sistemare)

---
+++ Assets/CustomSkin.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterCustomizationMenu : MonoBehaviour
+{
+    public TextMeshProUGUI skinNameText;
+    public GameObject characterPrefab;
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin=0;
+    //public GameObject playerSkin;
+
+    private void Start()
+    {
+        UpdateSkinName();
+    }
+
+    public void NextSkin()
+    {
+        selectedSkin = (selectedSkin + 1) % skins.Count;
+        UpdateSkinName();
+        sr.sprite = skins[selectedSkin];
+    }
+
+    public void PreviousSkin()
+    {
+        selectedSkin = (selectedSkin - 1 + skins.Count) % skins.Count;
+        UpdateSkinName();
+        sr.sprite = skins[selectedSkin];
+    }
+
+    private void UpdateSkinName()
+    {
+        skinNameText.text = skins[selectedSkin].ToString().Remove(skins[selectedSkin].ToString().IndexOf(" "));
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
